7decd749cd4684ac5c6b3c6bdc0d340a6b9b4a13
Jose Wollinger <jose.wollinger@gmail.com>
2019-04-25 20:09:50 -0300

    Implementação de crud de eventos

---
+++ backend-simple-pass-events/eventoService/handler.js
@@ -0,0 +1,79 @@
+'use strict';
+
+const eventCreate = require('./event-create.js/index.js');
+const eventReadAll = require('./event-read-all.js/index.js');
+const eventReadOne = require('./event-read-one.js/index.js');
+const eventUpdate = require('./event-update.js/index.js');
+const eventDelete = require('./event-delete.js/index.js');
+
+module.exports.create = (event, context, callback) => {
+  eventCreate(event, (error, result) => {
+    const response = {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin" : "*"
+      },
+      body: JSON.stringify(result),
+    };
+
+    context.succeed(response);
+  });
+};
+
+module.exports.readAll = (event, context, callback) => {
+  eventReadAll(event, (error, result) => {
+    const response = {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin" : "*"
+      },
+      body: JSON.stringify(result),
+    };
+
+    context.succeed(response);
+  });
+};
+
+module.exports.readOne = (event, context, callback) => {
+  eventReadOne(event, (error, result) => {
+    const response = {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin" : "*"
+      },
+      body: JSON.stringify(result),
+    };
+
+    context.succeed(response);
+  });
+};
+
+module.exports.update = (event, context, callback) => {
+  eventUpdate(event, (error, result) => {
+    const response = {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin" : "*"
+      },
+      body: JSON.stringify(result),
+    };
+
+    context.succeed(response);
+  });
+};
+
+module.exports.delete = (event, context, callback) => {
+  eventDelete(event, (error, result) => {
+    const response = {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin" : "*"
+      },
+      body: JSON.stringify(result),
+    };
+
+
+
+    context.succeed(response);
+  });
+};

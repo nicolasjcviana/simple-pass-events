f2b6c32fa443522795ad768343cfa3c81af9dda2
nicolasjcviana <nicolasjcviana@gmail.com>
2019-05-01 09:09:20 -0300

    Backend usuários

--- backend-simple-pass-events/usuarioService/handler.js
+++ backend-simple-pass-events/usuarioService/handler.js
@@ -1,14 +1,79 @@
 'use strict';
 
-module.exports.hello = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
+const usersCreate = require('./users-create.js');
+const usersReadAll = require('./users-read-all.js');
+const usersReadOne = require('./users-read-one.js');
+const usersUpdate = require('./users-update.js');
+const usersDelete = require('./users-delete.js');
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+module.exports.create = (event, context, callback) => {
+  usersCreate(event, (error, result) => {
+    const response = {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin" : "*"
+      },
+      body: JSON.stringify(result),
+    };
+
+    context.succeed(response);
+  });
+};
+
+module.exports.readAll = (event, context, callback) => {
+  usersReadAll(event, (error, result) => {
+    const response = {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin" : "*"
+      },
+      body: JSON.stringify(result),
+    };
+
+    context.succeed(response);
+  });
+};
+
+module.exports.readOne = (event, context, callback) => {
+  usersReadOne(event, (error, result) => {
+    const response = {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin" : "*"
+      },
+      body: JSON.stringify(result),
+    };
+
+    context.succeed(response);
+  });
+};
+
+module.exports.update = (event, context, callback) => {
+  usersUpdate(event, (error, result) => {
+    const response = {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin" : "*"
+      },
+      body: JSON.stringify(result),
+    };
+
+    context.succeed(response);
+  });
+};
+
+module.exports.delete = (event, context, callback) => {
+  usersDelete(event, (error, result) => {
+    const response = {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin" : "*"
+      },
+      body: JSON.stringify(result),
+    };
+
+
+
+    context.succeed(response);
+  });
 };

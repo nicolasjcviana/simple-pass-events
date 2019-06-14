7f4ec566ecfe4b6bb39a9fe7a14a5522ad7f3fce
Nicolas <nicolasjcviana@gmail.com>
2019-06-13 21:04:50 -0300

    Remoção de arquivo

--- frontend/simple-pass/src/app/app.api.ts
+++ frontend/simple-pass/src/app/app.api.ts
@@ -1,13 +1,1 @@
 export const API = 'AWS_KEY'
-export const ENDPOINTS = {
-    UPLOAD_FILE_S3: 'https://l07f72j09g.execute-api.us-east-1.amazonaws.com/dev/uploadFile',
-    ANALYSE_FACE: 'https://jbmzcipf9g.execute-api.us-east-1.amazonaws.com/dev/analyseFace',
-    // UPLOAD_FILE_S3: 'http://localhost:3000/uploadFile',
-    // ANALYSE_FACE: 'http://localhost:3031/analyseFace',
-
-    USER: 'https://g5vk5bg3nl.execute-api.us-east-1.amazonaws.com/dev/user',
-    EVENT: 'https://hxlq4shscd.execute-api.us-east-1.amazonaws.com/dev/event',
-    USER_IMAGE:  'http://localhost:3000/checkFace'//'https://o8uxggxled.execute-api.us-east-1.amazonaws.com/dev/checkFace'
-}
-
-export const BUCKET_NAME = 'simple-pass-images'
